c02c942d11f0e5b5663acb702ce21c9f271fa560
amauryD <deflorenne.amaury@gmail.com>
2020-12-15 11:33:08 +0100

    copie controllers

--- express-version/controllers/films.js
+++ express-version/controllers/films.js
@@ -1,25 +1,54 @@
-// ajoute la méthode details à l'export du module
-exports.details = (req,res) => {
-    console.log(req.url);
-    res.send(req.url);
+const movies = require("../models/movies");
+const renderPage = require("../utils/render-page");
+const comments = require("../models/comments");
+
+exports.details = async (request,response) => {
+    // ex url : http://localhost:8080/films/details?id=573a13fbf29313caabdee03b
+    // En fonction du paramètre passé dans la route , récupérer le film correspondant 
+    // ainsi que ses commmentaires et l'afficher dans un template moustache
+    const movie = await movies
+        .findOne({_id : request.query.id})
+        .populate("comments"); //va charger la référence (virtuelle dans ce cas-ci)
+
+    // v v v v v v v v v v v 🎅 🎅 🎅 🎅 🎅 😂 😂 
+    // POPULATE = CHARGER REFERENCE , (COMME CHARGER UNE RELATION EN FAIT)
+    // (important de spécifier dans le schéma la référence avec le type mongoose.Schema.Types.ObjectId)
+    // ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ 🎅 🎅 🎅 🎅 🎅 😂 😂
+
+    // méthode 1 : récupérer les commentaires en fonction du movie_id , "méthode manuelle"
+    // const commentsList = await comments.find({
+    //     movie_id : movie._id
+    // });
+
+    // console.log(commentsList);
+
+    // on recherche les films liés au movie
+    // const comments = await movies.find({
+    //     movie_id : request.query.id
+    // });
+    // movie.comments = comments;
+
+    const html = await renderPage("movie-details",{
+        movie : movie,
+        title : movie.title,
+        // comments : commentsList , si propriété virtual , pas besoin de le passer séparément
+    });
+
+    response.send(html);
 }
 
-// ajoute la méthode list à l'export du module
-exports.list = (req,res) => {
-    console.log(req.url);
-    res.send(req.url);
-}
+exports.list = async (request,response) => {
+    const films = await movies
+        .find()
+        .limit(10)
+        .populate("comments"); //  je vais charger mes commentaires pour mes films
 
-// OU cette synthaxe
-/*
-module.exports = {
-    details : (req,res) => {
-        console.log(req.url);
-        res.send(req.url);
-    },
-    list : (req,res) => {
-        console.log(req.url);
-        res.send(req.url);
-    }
+    const html = await renderPage("films",{
+        currentDate : new Date().toDateString(), //prend la date actuelle et la convertis en chaine lisible
+        // la variable film sera disponible  dans le template en tant que movies
+        movies : films,
+        title : "Nos films 🎥"
+    });
+
+    response.send(html);
 }
-*/
--- express-version/controllers/theaters.js
+++ express-version/controllers/theaters.js
@@ -1,8 +1,18 @@
-module.exports = () => {
-    console.log("theaters");
-}
+// récupérez les cinemas 
+// affichez l'addresse complète du cinéma 
+const theaters = require("../models/theater");
+const renderPage = require("../utils/render-page");
 
-// module.exports = {
-//     details : () => {},
-//     list : () => {}
-// }
+module.exports = async (req,res) => {
+    const allTheaters = await theaters
+        .find()
+        .limit(10);
+
+    const html = await renderPage("theaters",{
+        theaters : allTheaters,
+        title : "Nos cinémas"
+    });
+
+    // la fonction controller renvoie un résultat
+    res.send(html);
+}
