818e845184770ee2b86a9192562c66287395dc96
Amaury Deflorenne <amaury@triptyk.eu>
2020-12-15 09:03:45 +0100

    ivan

--- rappel/index.js
+++ rappel/index.js
@@ -3,7 +3,9 @@ const module1 = require("./module2");
 // console.log(module1);
 // console.log(module1.details);
 // console.log(module1.index);
-// console.log(module1.ivanIsAwesome);
+
+
+// blala
 
 module1.index();
 module1.details();
